8cb965c0dbfdb776aa14f7c362ac04298e522bc9
Praneet207 <92368151+Praneet207@users.noreply.github.com>
2023-04-11 20:01:00 +0530

    Update update.py

--- update.py
+++ update.py
@@ -23,7 +23,12 @@ try:
 except:
     pass
 
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
+if len(BOT_TOKEN) == 0:
+    log_error("BOT_TOKEN variable is missing! Exiting now")
+    exit(1)
 
+bot_id = int(BOT_TOKEN.split(':', 1)[0])
 
 DATABASE_URL = environ.get('DATABASE_URL', '')
 if len(DATABASE_URL) == 0:
